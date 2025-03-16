1df79ff1b72c90a1e099e091dbf05bc76f3f121f
Michael Yang <yql.michael@gmail.com>
2025-03-16 10:11:01 +0800

    cf2050f max mod equality

--- cf/math/2050f.cpp
+++ cf/math/2050f.cpp
@@ -1,56 +1,52 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+#define rep(i, a, b) for (int (i) = (a); (i) < (b); (i)++) 
+
 using ll = long long;
-using ull = unsigned long long;
 using pii = pair<int, int>;
-using vvi = vector<vector<int>>;
 using vi = vector<int>;
 
-#define umap unordered_map
-
+const int MAX_K = 20;
 const int N = 2e5 + 5;
-int a[N];
-const int LOG = 20; 
-int st[LOG][N]; // sparse table
+int st[MAX_K][N];
 
-// 2^10 = 1e3
-// 2^20 = 1e6
-// 2^19 = ~5e5
-
-void build(int n) {
-  for (int i = 0; i < n - 1; i++) { // for interval size of 2^1 = 2
-    // st[1][i] = ;
-  }
-  for (int pw = 2; pw <= n; pw *= 2) {
-
-  }
-}
-
-int query(int l, int r) {
-
-}
 
 void solve() {
-  int n, q;
-  cin >> n >> q;
-  memset(st, 0, LOG * (n + 1));
-  for (int i = 0; i < n; i++) {
-    cin >> a[i];
+	int n, q;
+	cin >> n >> q;
+	vi a(n), diff(n-1);
+	rep(i, 0, n) cin >> a[i];
+  rep(i, 0, n - 1) {
+    diff[i] = abs(a[i + 1] - a[i]);
+    st[0][i] = diff[i];
   }
-  build(n);
-  for (int i = 0; i < q; i++) {
-    int l, r;
-    cin >> l >> r;
-    cout << query(l, r) << " ";
-  }
-  cout << endl;
-}
+	for (int i = 1; i < MAX_K; i++) {
+		for (int j = 0; j + (1 << i - 1) < n - 1; j++) {
+			st[i][j] = gcd(st[i - 1][j], st[i - 1][j + (1 << i - 1)]); 
+		}
+	}
+	int l, r;
+	while (q--) {
+		cin >> l >> r;
+    if (l == r) {
+      cout << 0 << endl;
+      continue;
+    }
+		int sz = r - l;
+		l--;
+    r -= 2;
+		int level = 0;
+		while ((1 << level + 1) <= sz) level++;
+		int GCD = gcd(st[level][l], st[level][r - (1 << level) + 1]);
+		cout << GCD << endl;
+	}
+} 
 
 int main() {
-  cin.sync_with_stdio(false);
-  cin.tie(nullptr);
-  int t;
-  cin >> t;
-  while (t--) solve();
+	ios::sync_with_stdio(false);
+	cin.tie(nullptr); 
+	int t;
+	cin >> t;
+	while (t--) solve();
 }
