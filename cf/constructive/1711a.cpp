0411920ed263f91b5943408fe0261294b2d93792
Michael Yang <yql.michael@gmail.com>
2025-05-31 07:58:25 +0800

    cf 1711a, 1712b

---
+++ cf/constructive/1711a.cpp
@@ -0,0 +1,35 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define rep(i, a, b) for (int i = a; i < b; i++) 
+#define LSOne(S) ((S) & -(S)) 
+#define all(x) (x).begin(), (x).end()
+#define debug
+ 
+#ifdef debug
+#define db(x) cout << #x " = " << x << endl;
+#define bp(x) cout << #x << endl;
+#else 
+#define db(x) 
+#define bp(x)
+#endif
+
+using ll = long long;
+using pii = pair<int, int>;
+using vi = vector<int>;
+using vvi = vector<vector<int>>;
+
+void solve() {
+	int n;
+	cin >> n;
+	rep(i, 2, n + 1) cout << i <<  " ";
+	cout << 1 << endl;
+} 
+
+int main() {
+	ios::sync_with_stdio(false);
+	cin.tie(nullptr); 
+	int t;
+	cin >> t;
+	while (t--) solve();
+}
---
+++ cf/constructive/1712b.cpp
@@ -0,0 +1,44 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define rep(i, a, b) for (int i = a; i < b; i++) 
+#define LSOne(S) ((S) & -(S)) 
+#define all(x) (x).begin(), (x).end()
+#define debug
+#define println cout << endl; 
+
+#ifdef debug
+#define db(x) cout << #x " = " << x << endl;
+#define bp(x) cout << #x << endl;
+#else 
+#define db(x) 
+#define bp(x)
+#endif
+
+using ll = long long;
+using pii = pair<int, int>;
+using vi = vector<int>;
+using vvi = vector<vector<int>>;
+
+void solve() {
+	int n;
+	cin >> n;
+	int j = 1;
+	if (n & 1) {
+		j = 2;
+		cout << 1 << " ";
+	}
+	for (int i = 0; i < n / 2; i++) {
+		cout << j + 1 << " " << j << " ";
+		j += 2;
+	}
+	println
+} 
+
+int main() {
+	ios::sync_with_stdio(false);
+	cin.tie(nullptr); 
+	int t;
+	cin >> t;
+	while (t--) solve();
+}
