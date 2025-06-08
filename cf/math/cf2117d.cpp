52e08d8ce150b32799c7496e08e7da956fdafe5d
Michael Yang <yql.michael@gmail.com>
2025-06-09 03:01:09 +0800

    cf2117 a-d

---
+++ cf/math/cf2117d.cpp
@@ -0,0 +1,52 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define rep(i, a, b) for (int i = a; i < b; i++) 
+#define LSOne(S) ((S) & -(S)) 
+#define all(x) (x).begin(), (x).end()
+#define debug
+ 
+#ifdef debug
+#define db(x) cout << #x " = " << x << endl;
+#define bp(x) cout << #x << endl;
+#else 
+#define db(x) 
+#define bp(x)
+#endif
+
+using ll = long long;
+using pii = pair<int, int>;
+using vi = vector<int>;
+using vvi = vector<vector<int>>;
+using vll = vector<ll>;
+using vvll = vector<vector<ll>>;
+using vb = vector<bool>;
+
+void solve() {
+	int n;
+	cin >> n;
+	vi a(n);
+	rep(i, 0, n) cin >> a[i];
+	set<int> need, seen;
+	int ans = 0;
+	rep(i, 0, n) {
+		seen.insert(a[i]);
+		if (need.find(a[i]) != need.end()) {
+			need.erase(a[i]);
+		}
+		if (need.empty()) {
+			ans++;
+			need = seen;
+			seen.clear();
+		} 
+	}
+	cout << ans << endl;
+} 
+
+int main() {
+	ios::sync_with_stdio(false);
+	cin.tie(nullptr); 
+	int t;
+	cin >> t;
+	while (t--) solve();
+}
