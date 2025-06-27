d37251354656d34cbbaf7686ff1a9b2a89b0da21
Michael Yang <yql.michael@gmail.com>
2025-06-28 00:51:30 +0800

    cp4 complete search g
    * kattis
    gradecurving
    owlandfox
    parking2
    savingforretirement

---
+++ cp4/complete-search/g-kattis-parking2.cpp
@@ -0,0 +1,40 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+#define rep(i, a, b) for (int i = a; i < b; i++)
+#define LSOne(S) ((S) & -(S))
+#define all(x) (x).begin(), (x).end()
+#define debug
+
+#ifdef debug
+#define db(x) cout << #x " = " << x << endl;
+#define bp(x) cout << #x << endl;
+#else
+#define db(x)
+#define bp(x)
+#endif
+
+using ll = long long;
+using pii = pair<int, int>;
+using vi = vector<int>;
+using vvi = vector<vector<int>>;
+using vll = vector<ll>;
+using vvll = vector<vector<ll>>;
+using vb = vector<bool>;
+
+void solve() {
+    int n;
+    cin >> n;
+    vi a(n);
+    rep(i, 0, n) cin >> a[i];
+    sort(all(a));
+    cout << (a[n - 1] - a[0]) * 2 << endl;
+}
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    int t;
+    cin >> t;
+    while (t--) solve();
+}
---
+++ cp4/complete-search/g-kattis-savingforretirement.cpp
@@ -0,0 +1,30 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+#define rep(i, a, b) for (int i = a; i < b; i++)
+#define LSOne(S) ((S) & -(S))
+#define all(x) (x).begin(), (x).end()
+#define debug
+
+#ifdef debug
+#define db(x) cout << #x " = " << x << endl;
+#define bp(x) cout << #x << endl;
+#else
+#define db(x)
+#define bp(x)
+#endif
+
+using ll = long long;
+using pii = pair<int, int>;
+using vi = vector<int>;
+using vvi = vector<vector<int>>;
+using vll = vector<ll>;
+using vvll = vector<vector<ll>>;
+using vb = vector<bool>;
+
+int main() {
+    int b, br, bs, a, as;
+    cin >> b >> br >> bs >> a >> as;
+    int k = ((br - b) * bs) / as + 1; 
+    cout << a + k << endl;
+}
